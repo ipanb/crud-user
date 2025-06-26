bc7cac0b8b7cee95110c1bb804bce9c21d8ac6aa
Ipan Badruzzaman <ipannbzn1@gmail.com>
2025-06-26 08:24:24 +0700

    cape bug trus

--- src/frankenphp/index.php
+++ src/frankenphp/index.php
@@ -23,6 +23,7 @@ do {
             $path = parse_url($requestUri, PHP_URL_PATH);
             $pathSegments = explode('/', trim($path, '/'));
             $path = '/' . (end($pathSegments) ?: '');
+            require __DIR__ . '/functions.php';
             
             switch ($path) {
                 case '':
